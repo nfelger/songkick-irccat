a1ddbbfaf93dfeb90e1e670b29fdc24e9f098b19
Niko Felger <niko.felger@gmail.com>
2011-06-16 15:28:39 +0100

    You're wizard, Harry

--- cmd_handler.sh
+++ cmd_handler.sh
@@ -130,6 +130,69 @@ class RandomAnswer
   end
 end
 
+# Thanks to http://www.youtube.com/watch?v=0mQaIMYIvYU
+class Wizard < RandomAnswer
+  def initialize(character)
+    @character = character
+  end
+
+  private
+
+  def answers
+    answer_corpus = {
+      "harry" => [
+        "I'm a... WHAT?!",
+        "I'm a what?!",
+        "I'm a WIZARD?!",
+        "but I'm just Harry.",
+        "but I'm just Harry!",
+        "Listen here Hagrid I'm just Harry!",
+        "I'm not a wizard, Hagrid, I'm just Harry!",
+	"No Hagrid, I'm just Harry!",
+	"A WIZARD?! I'm just Harry!",
+	"I'm not a wizard, Hagrid, I'm just Harry.",
+	"I'm not a wizard, Hagrid!",
+	"Listen here Hagrid you FAT OAF! I'm not a FUCKING WIZARD!",
+	"I don't give a FUCK you FAT HAIRY BASTARD! I'm not a FUCKING WIZARD!",
+	"HAGRID, y'er pushing me over the FUCKING line!",
+	"I'm a WHAT?!",
+	"Hagrid I've been through this I don't give a BLOODY FUCK WHAT YOU THINK.",
+	"I'LL FUCKING SET YER BEARD ON FIRE!",
+	"I'll fucking NAW yer ARM off, Hagrid!",
+      ],
+      "hagrid" => [
+        "You're a wizard Harry.",
+        "Harry, you're a wizard.",
+        "A wizard, Harry.",
+        "Yes Harry, you're a wizard.",
+        "Well, \"Just Harry\", you're a wizard.",
+        "No, \"Just Harry\", you - are a wizard!",
+        "NO! Harry, you are a wizard!",
+        "Listen Harry, you are a wizard!",
+	"Harry, for god's sake, you are a wizard!",
+	"Nooo, \"Just Harry\"! You're a wizard.",
+	"Noooooo. Just Harry. You are a wizard.",
+	"HARRY, you are a wizard!",
+	"For god's sake Harry, what is with this language?! You're a FUCKING WIZARD.",
+	"Listen Harry, you're going to go to Hogwarts and do SPELLS and SHIT. And you're going to be FUCKING pleased about it!",
+	"My fucking WHAT?!?!",
+	"No I'm not. You - are a wizard! You're going to go to Hogwarts, you're going to do spells, you'll get a wand, you'll get a fucking owl, it'll deliver your mail - DEAL WITH IT. YA TWAT.",
+	"I did that when I was younger, and that was a bad move. You, are a wizard.",
+	"YOU'RE A WIZARD HARRY, FOR FUCK SAKE, LISTEN TO MEH!",
+	"I'LL FUCKING BURST YE'",
+	"Right you, you little wank stain. If you don't get your act together, I'm gonna drag you to hogwarts. You'll get a wand, you'll get an owl that'll deliver your SHITEY meal and that'll be that and you'll enjoy it ya' sch-ch-ff-chhssh.",
+	"I'll PUMP ye' SILLY.",
+	"LET'S GO RIGHT NOW BRING IT ON YA LITTLE WANK",
+      ]
+    }
+    if @character
+      answer_corpus[@character]
+    else
+      answer_corpus.values.flatten
+    end 
+  end
+end
+
 COUNTERS = ['wtf','lol','voot','sigh']
 LISTS    = ['lunch']
 
@@ -153,6 +216,10 @@ command =
 
   elsif args.any?{ |arg| arg =~ /\bcats?\b/ }
     ApiCurl.new('http://gdata.youtube.com/feeds/api/videos?q=_-#QUERY#-_&orderby=relevance&max-results=1&alt=json', args.join(' ')) { |response| JSON.parse(response)["feed"]["entry"][0]["link"][0]["href"] }
+
+  elsif ["harry", "wizard", "hagrid"].include?(args.first.downcase)
+    character = args.first.downcase
+    Wizard.new(character == 'wizard' ? nil : character)
 
   else
     RandomAnswer.new
